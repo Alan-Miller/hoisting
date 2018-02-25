bb1f18a205350e0561c77291783121698f57b8d5
Alan-Miller <alan.ashman@gmail.com>
2018-02-25 00:09:26 -0700

    edits

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 require('dotenv').config();
-const { port } = process.env;
+const port = 3456;
 const app = require('express')().listen(port, console.log(`Listening on ${port}.`));
 
-function hoist() {
 
-    let test = 5;
-    const getAv = () => {
-        
-    }
-    return test;
+function getAv() {
+    console.log('I know five is', five);
+    console.log('I know ten is', ten);
+    const ten = 10;
+    const args = [...arguments];
+    return args.reduce((a, b) => a + b) / args.length;
 }
 
 
+const setAv = () => {
+    console.log('I know five is', five);
+    console.log('I know ten is', ten);
+    var ten = 10;
+    const args = [...arguments];
+    return args.reduce((a, b) => a + b) / args.length;
+}
 
-console.log(hoist());
+
+console.log('I log five is', five, '\navg is', getAv(1, 2, 3, 4, 5));
+var five = 5;
